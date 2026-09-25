49f45812400f8e4b802e7679c4900b4a1e94793b
lindapkhasuvor <linda.pkhasuvor@gmail.com>
2026-09-26 03:43:59 +0700

    Add MyBabyCards catalog config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// Вставьте сюда только Dropbox App Key.
+// НЕ добавляйте App Secret или access token.
+const CATALOG = {
+  APP_KEY: "",
+  speech: "https://www.dropbox.com/scl/fo/17uz4dtrppvrsoqu4lewu/AA1J10NDck4WPpY9LR8Hnv4?rlkey=bpjb9y7d1p04u3vg0x13nwbel&dl=0",
+  firstWords: "https://www.dropbox.com/scl/fo/fm5krmajx6sdash12vcn8/AB11kYE_vF4pksnDAF-kP2o?rlkey=qx322eg6e5znqmsd2gn8e3np&dl=0",
+  emotions: "https://www.dropbox.com/scl/fo/o4ke6pbhuip762tibkj6l/AP5P_M-KAxdax_BU_a45HaA?rlkey=35egacdqqikt63h8qwta79nkj&dl=0"
+};
